a7411ce32424b41c50e07594281aeeed46aa5011
yonatan895 <jony04y01@gmail.com>
2024-01-18 20:33:41 +0200

    Improved testing with implementation of the Testable trait

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
 pub enum QemuExitCode {
     Success = 0x10,
     Failed = 0x11,
@@ -45,13 +43,28 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
 
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 
@@ -61,9 +74,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
 }
 
 
