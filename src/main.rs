e11aae5b35130570d32fa1a9a24a8491ef50091c
yonatan895 <jony04y01@gmail.com>
2024-01-18 20:57:57 +0200

    Added 'tests' folder for integration testing

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,14 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(custom_test_frameworks)] // Because #![no_std]
-#![test_runner(crate::test_runner)]
+#![test_runner(yonti_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 
-mod serial;
-mod vga_buffer;
 
 
 use core::panic::PanicInfo;
-
-
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-
-}
-
-
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
+use yonti_os::println;
 
 
 #[no_mangle]
@@ -88,3 +21,16 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    yonti_os::test_panic_handler(info)
+}
---
+++ tests/basic_boot.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(yonti_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    yonti_os::test_panic_handler(info)
+}
+
+
+use yonti_os::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
