2c046a4e5a2942cef5b954ec92b7ae9574101a47
yonatan895 <jony04y01@gmail.com>
2024-01-18 19:08:34 +0200

    Added testing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+}
 use core::panic::PanicInfo;
 
 /// This function is called on panic
@@ -10,14 +21,22 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
 mod vga_buffer;
 
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // This function is the entry point, since the linker looks for a function name '_start' by
-    // default
     println!("Hello World{}", "!");
-    panic!("Some panic messege");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
+
